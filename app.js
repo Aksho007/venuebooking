24f31bed10ba78ee1714e48300466473d9ed2cb9
AakarshanSingh <95141279+AakarshanSingh@users.noreply.github.com>
2022-07-23 01:06:45 +0530

    Auth added!

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const Booking = require("./schema/venueSchema");
 const user = require("./schema/loginSchema");
 const timeSecConv = require("time-to-seconds");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const MongoURL = "mongodb://localhost:27017/booking";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,6 +18,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "hbs");
+
+const isAuth = (req, res, next) => {
+  if (req.session.isAuth) {
+    next();
+  } else {
+    res.render("login", {
+      success: "Please login before booking",
+    });
+  }
+};
+
+// Session
+
+const store = new MongoDBStore({
+  uri: MongoURL,
+  collection: "users",
+});
+
+app.use(
+  session({
+    secret: "key that will sign cookie",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+// Main Login page
 
 app.get("/", function (req, res) {
   res.render("login", { success: "" });
@@ -31,9 +62,8 @@ app.post("/", async (req, res) => {
       loginUserEmail.loginPassword
     );
 
-    console.log(isMatch);
-    // user.findOne({ loginEmail: loginEmail }, (err, user) => {
     if (isMatch) {
+      req.session.isAuth = true;
       res.status(201).render("venueBooking");
     } else {
       res.render("login", {
@@ -47,51 +77,53 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.get("/booking", function (req, res) {
+// Venue Booking Page
+
+app.get("/booking", isAuth, function (req, res) {
   res.render("venueBooking", { success: "" });
 });
 
 app.post("/booking", async (req, res) => {
   try {
-    var TimeInSecFromR = timeSecConv(`${req.body.timeFrom}:0`);
-    var TimeInSecToR = timeSecConv(`${req.body.timeTo}:0`);
+    var TimeInSecFrom = timeSecConv(`${req.body.timeFrom}:0`);
+    var TimeInSecTo = timeSecConv(`${req.body.timeTo}:0`);
 
-    var result = await Booking.find({
-      $and: [{ venue: `${req.body.venue}` }, { date: `${req.body.date}` }],
-    });
-    // console.log(result);
-
-    var resultCount = await Booking.find({
-      $and: [{ venue: `${req.body.venue}` }, { date: `${req.body.date}` }],
-    }).count();
-    // console.log(resultCount);
-
-    var newbo = await Booking.find({
-      date: `${req.body.date}`,
-      $and: [
-        { TimeInSecFrom: { $gt: `${TimeInSecToR}` } },
-        { TimeInSecTo: { $lt: `${TimeInSecFromR}` } },
-      ],
-    });
-    console.log(newbo);
-
-    // const bookVenue = new Booking({
-    //   name: req.body.name,
-    //   email: req.body.email,
-    //   number: req.body.number,
-    //   venue: req.body.venue,
-    //   timeFrom: req.body.timeFrom,
-    //   timeTo: req.body.timeTo,
-    //   TimeInSecFrom:TimeInSecFrom,
-    //   TimeInSecTo: TimeInSecTo,
-    //   date: req.body.date,
-    //   purpose: req.body.purpose,
+    // var result = await Booking.find({
+    //   $and: [{ venue: `${req.body.venue}` }, { date: `${req.body.date}` }],
     // });
-    // const venueBooked = await bookVenue.save();
+    // // console.log(result);
 
-    // res.status(201).render("venuebooking", {
-    //   success: "You will be informed via email about your booking.",
+    // var resultCount = await Booking.find({
+    //   $and: [{ venue: `${req.body.venue}` }, { date: `${req.body.date}` }],
+    // }).count();
+    // // console.log(resultCount);
+
+    // var newbo = await Booking.find({
+    //   date: `${req.body.date}`,
+    //   $and: [
+    //     { TimeInSecFrom: { $gt: `${TimeInSecToR}` } },
+    //     { TimeInSecTo: { $lt: `${TimeInSecFromR}` } },
+    //   ],
     // });
+    // console.log(newbo);
+
+    const bookVenue = new Booking({
+      name: req.body.name,
+      email: req.body.email,
+      number: req.body.number,
+      venue: req.body.venue,
+      timeFrom: req.body.timeFrom,
+      timeTo: req.body.timeTo,
+      TimeInSecFrom:TimeInSecFrom,
+      TimeInSecTo: TimeInSecTo,
+      date: req.body.date,
+      purpose: req.body.purpose,
+    });
+    const venueBooked = await bookVenue.save();
+
+    res.status(201).render("venuebooking", {
+      success: "You will be informed via email about your booking.",
+    });
     console.log("Booking Successful");
   } catch (error) {
     res.status(400).send(error);
@@ -99,7 +131,11 @@ app.post("/booking", async (req, res) => {
   }
 });
 
+// Register user page
+
 app.get("/register", function (req, res) {
+  req.session.isAuth = true;
+  console.log(req.session);
   res.render("register", { success: "" });
 });
 
@@ -113,7 +149,10 @@ app.post("/register", async (req, res) => {
         loginEmail: req.body.registerEmail,
         loginPassword: req.body.registerPassword,
       });
+
       const newUser = await registerUser.save();
+
+      console.log("lol", newUser);
       res.status(201).render("register", {
         success: "User registered",
       });
@@ -129,6 +168,8 @@ app.post("/register", async (req, res) => {
     });
   }
 });
+
+// Server running port
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
